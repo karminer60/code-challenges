989f8f22ca95ae7ce51b8363828c90c089ba9dfc
karminer60 <karminer60@gmail.com>
2024-11-10 16:20:16 -0800

    first draft of solution

--- Roman-to-Integer.js
+++ Roman-to-Integer.js
@@ -17,13 +17,24 @@ I can be placed before V (5) and X (10) to make 4 and 9.
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
- * @param {string} 
+ /**
+ * @param {string} s
  * @return {number}
  */
 var romanToInt = function(s) {
     //create an object to map Roman numerals to integers
     //then create code for rules of addition and subtraction
+    let output = 0
     const RomanToInteger = {"I":1, "V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-    // if()
-    
+    for(let i = 0; i < s.length; i++){
+        if(i === 0){
+            output += RomanToInteger[s[i]]
+        }
+        else if(s[i] <= s[i - 1]){
+            output += (RomanToInteger[s[i - 1]] - RomanToInteger[s[i]])
+        }else{
+            output += RomanToInteger[s[i]]
+        }
+    }
+    return output
 };
