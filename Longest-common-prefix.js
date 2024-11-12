dc9c4c8824517da5c81763d26753e6331c3dfe9c
karminer60 <karminer60@gmail.com>
2024-11-12 14:44:19 -0800

    solution draft

--- Longest-common-prefix.js
+++ Longest-common-prefix.js
@@ -17,8 +17,21 @@ Explanation: There is no common prefix among the input strings.
  * @param {string[]} strs
  * @return {string}
  */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
 var longestCommonPrefix = function(strs) {
     //iterate over the array of strings
     //iterate over each string and compare the letters from left to right
-    
+    for(let i = 0; true; i++){
+        let letter = strs[0][i]
+        for(let word of strs){
+            if(i >= word.length || letter !== word[i]){
+                return word.slice(0,i)
+            }
+        }
+    } 
+        
+          
 };
