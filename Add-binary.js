76ed8a469e06766712a23407d62d66f437740b30
karminer60 <karminer60@gmail.com>
2024-11-20 00:47:28 -0800

    addes pseudo code for Add Binary problem

---
+++ Add-binary.js
@@ -0,0 +1,14 @@
+/**
+ * Given two binary strings a and b, return their sum as a binary string.
+
+
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    //binary system goes from right to left
+    //the smallest number is on the right
+    //and the largest number is on the left
+    //the solution will be like the Plus One problem    
+};
