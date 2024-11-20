d970a0e7fef30d75e0f47d905108522646d18adc
karminer60 <karminer60@gmail.com>
2024-11-20 15:08:03 -0800

    adds initial logic for moving backwards from the end of a string

--- Add-binary.js
+++ Add-binary.js
@@ -10,5 +10,12 @@ var addBinary = function(a, b) {
     //binary system goes from right to left
     //the smallest number is on the right
     //and the largest number is on the left
-    //the solution will be like the Plus One problem    
-}
+    //the solution will be like the Plus One problem
+   
+    //use an index for this problem
+    for( let i = 0; i < a.length || i < b.length; i++ ){
+        //go from the end then move backwards by subtracting i
+        a.length - 1 - i
+
+    }  
+};
