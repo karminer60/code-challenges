c0bf6b3a96256d6a52f673736aa7af74cfcffb19
karminer60 <karminer60@gmail.com>
2024-11-20 16:21:07 -0800

    adds logic for adding and carrying when digit greater than one

--- Add-binary.js
+++ Add-binary.js
@@ -11,11 +11,25 @@ var addBinary = function(a, b) {
     //the smallest number is on the right
     //and the largest number is on the left
     //the solution will be like the Plus One problem
-   
+    let result = ""
+    let carry = 0
     //use an index for this problem
     for( let i = 0; i < a.length || i < b.length; i++ ){
         //go from the end then move backwards by subtracting i
-        a.length - 1 - i
-
-    }  
+        //one binary plus one binary equals 10 
+        let digit = Number(a[a.length - 1 - i] || "0") + Number(b[b.length - 1 -i] || "0") + carry
+        if(digit > 1){
+            carry = 1
+            digit = 0
+        }else{
+            carry = 0
+        }
+        result += digit
+    }
+    //do this at the end and apply it more generally
+    if(carry){
+        result += carry
+    } 
+   
+    return result.split("").reverse().join("");
 };
